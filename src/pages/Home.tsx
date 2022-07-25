7771d32106434e8194a27d1c70f23291d78bde85
Felipe Coury <felipe.coury@gmail.com>
2022-07-25 10:48:11 -0300

    feat: shows generated SQL with syntax highlighter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,37 @@
 import { Box, Button, Grid, GridItem, useColorMode } from '@chakra-ui/react';
 import { json } from '@codemirror/lang-json';
-import { sql } from '@codemirror/lang-sql';
 import CodeMirror from '@uiw/react-codemirror';
 import { useState } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import {
+  a11yDark,
+  a11yLight,
+} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function Home() {
   const { colorMode } = useColorMode();
   const [match, setMatch] = useState('{}');
-  const [query, setQuery] = useState('SELECT * FROM table');
+  const [query, setQuery] = useState(`SELECT _jsonb #- '{age_sum}'
+FROM (
+	SELECT
+		row_to_json(s_wrap)::jsonb AS _jsonb
+	FROM (
+		SELECT
+			_jsonb->'city' AS _id,
+			SUM((
+        CASE WHEN (_jsonb->'age' ? '$f')
+        THEN (_jsonb->'age'->>'$f')::numeric
+        ELSE (_jsonb->'age')::numeric END
+      )) AS age_sum
+		FROM (
+			SELECT *
+      FROM "db_test"."test_e4a3258c-29ad-49e0-aa41-327750e568e4"
+      WHERE _jsonb->'pick' = 'true'
+		) AS s_group
+		GROUP BY _id
+	) AS s_wrap
+) AS s_project;
+  `);
 
   return (
     <Box p={5}>
@@ -21,12 +45,14 @@ export default function Home() {
           />
         </GridItem>
         <GridItem w="100%">
-          <CodeMirror
-            value={query}
-            height="calc(40vh)"
-            theme={colorMode}
-            extensions={[sql()]}
-          />
+          <SyntaxHighlighter
+            language="sql"
+            style={colorMode === 'dark' ? a11yDark : a11yLight}
+            customStyle={{ height: 'calc(40vh)' }}
+            wrapLongLines={true}
+          >
+            {query}
+          </SyntaxHighlighter>
         </GridItem>
       </Grid>
       <Box mt={5}>
