fe415e6eaf1fe7bc728271ab418fe6a82ef2ec6a
Felipe Coury <felipe.coury@gmail.com>
2022-07-24 23:43:10 -0300

    feat: initial styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,40 @@
-import { Box, Grid, GridItem, Textarea } from '@chakra-ui/react';
+import { Box, Button, Grid, GridItem, useColorMode } from '@chakra-ui/react';
+import { json } from '@codemirror/lang-json';
+import { sql } from '@codemirror/lang-sql';
+import CodeMirror from '@uiw/react-codemirror';
+import { useState } from 'react';
 
 export default function Home() {
+  const { colorMode } = useColorMode();
+  const [match, setMatch] = useState('{}');
+  const [query, setQuery] = useState('SELECT * FROM table');
+
   return (
     <Box p={10}>
       <Grid templateColumns="repeat(2, 1fr)" gap={6}>
         <GridItem w="100%">
-          <Textarea h="calc(50vh)"></Textarea>
+          <CodeMirror
+            value={match}
+            height="calc(50vh)"
+            theme={colorMode}
+            extensions={[json()]}
+          />
         </GridItem>
         <GridItem w="100%">
-          <Textarea h="calc(50vh)"></Textarea>
+          <CodeMirror
+            value={query}
+            height="calc(50vh)"
+            theme={colorMode}
+            extensions={[sql()]}
+          />
         </GridItem>
       </Grid>
+      <Box mt={5}>
+        <Button colorScheme="gray">Run</Button>
+        <Button colorScheme="gray" ml={2}>
+          Clear
+        </Button>
+      </Box>
     </Box>
   );
 }
